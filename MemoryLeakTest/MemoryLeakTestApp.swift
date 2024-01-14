f3527e7fa264dfa345ee86342d1ea2ea450d33be
sikwan <yysskk99999@naver.com>
2024-01-15 00:30:06 +0900

    init

---
+++ MemoryLeakTest/MemoryLeakTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryLeakTestApp.swift
+//  MemoryLeakTest
+//
+//  Created by 양시관 on 1/15/24.
+//
+
+import SwiftUI
+
+@main
+struct MemoryLeakTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
